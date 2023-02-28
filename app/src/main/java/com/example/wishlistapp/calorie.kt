f9dad2831d2b8cc0e044485c63811ad80d08612a
rayisooo <ro27@njit.edu>
2023-02-28 03:05:29 -0500

    BIT FIT PT 1 DATABASE WORK

--- app/src/main/java/com/example/wishlistapp/calorie.kt
+++ app/src/main/java/com/example/wishlistapp/calorie.kt
@@ -7,3 +7,29 @@ class calorie(
     val url: TextView,
     val num: String,
     )
+
+@Entity
+data class User(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?
+)
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<User>
+
+    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
+            "last_name LIKE :last LIMIT 1")
+    fun findByName(first: String, last: String): User
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+}
